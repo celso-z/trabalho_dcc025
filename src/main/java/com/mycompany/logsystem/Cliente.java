be70148330a2d415489c1094adc091e336957251
Celso Zacarias <celso.zacarias@estudante.ufjf.br>
2023-11-23 14:53:07 -0300

    Esboço inicial de implementação da classe Cliente

---
+++ src/main/java/com/mycompany/logsystem/Cliente.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.logsystem;
+
+/**
+ *
+ * @author celso-z
+ */
+public class Cliente extends Usuario {
+    //Cargas x;
+    
+    
+    public Cliente(String nome) {
+        super(nome);
+    }
+    
+    /*public solicitarFrete(Integer Valor, Unidade orig, Unidade dest){
+    
+    */
+}
