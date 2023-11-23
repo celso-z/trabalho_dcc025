7bea11b9b9b4d9854b4bc43c011c923e2afafda0
Celso Zacarias <celso.zacarias@estudante.ufjf.br>
2023-11-23 14:54:35 -0300

    Esboço inicial de implementação da trilha de execução principal (main) do projeto no arquivo LogSystem.java

---
+++ src/main/java/com/mycompany/logsystem/LogSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Um Sistema de gestão logística
+ */
+
+package com.mycompany.logsystem;
+
+/**
+ *
+ * @author Celso Zacarias da Silva Junior 202076003
+ */
+public class LogSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
