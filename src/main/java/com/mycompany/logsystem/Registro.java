210148e54cb5d8caa1a6d8909e06b2c1f7c6cc17
Celso Zacarias <celso.zacarias@estudante.ufjf.br>
2023-12-01 23:47:58 -0300

    Esboço básico da classe abstrata registro

--- src/main/java/com/mycompany/logsystem/Registro.java
+++ src/main/java/com/mycompany/logsystem/Registro.java
@@ -3,13 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.logsystem;
+import java.io.*;
+import java.util.List;
 
 
-public class Registro {
-    public Registro(){
-        
+
+public abstract class Registro {
+    final String DATA_DIR;
+    public abstract void EscreveRegistro(List<Registro> itens);
+    public abstract Registro LeRegistro();
+
+    public Registro(String diretorioSalvamento) {
+        this.DATA_DIR = "data" + diretorioSalvamento;
     }
-    public Salva 
-    
+            
     
 }
