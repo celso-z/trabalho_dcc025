bc63f81f8deef19c7526fb08b2f537bf7966b055
Dhayana N. Silva <dhayana@on2.dev>
2023-12-03 15:03:14 -0300

    Adiciona tela de login

--- src/main/java/views/Login.java
+++ src/main/java/views/Login.java
@@ -4,10 +4,58 @@
  */
 package views;
 
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
 /**
  *
  * @author dhayana
  */
-public class Login {
-    
+public class Login extends Janela {
+
+    JLabel label_usuario;
+    JTextField text_usuario;
+
+    JLabel label_senha;
+    JPasswordField text_senha;
+
+    JButton botao_entrar;
+
+    public Login() {
+
+        label_usuario = new JLabel("Usuário");
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        painel.add(label_usuario, gbc);
+
+        text_usuario = new JTextField();
+        text_usuario.setPreferredSize(new Dimension(INPUT_WIDTH, INPUT_HEIGHT));
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        painel.add(text_usuario, gbc);
+
+        label_senha = new JLabel("Senha");
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        painel.add(label_senha, gbc);
+
+        text_senha = new JPasswordField();
+        text_senha.setPreferredSize(new Dimension(INPUT_WIDTH, INPUT_HEIGHT));
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        painel.add(text_senha, gbc);
+
+        botao_entrar = new JButton("Entrar");
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        painel.add(botao_entrar, gbc);
+
+        add(painel);
+    }
 }
