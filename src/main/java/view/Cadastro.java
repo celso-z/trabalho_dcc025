4f0389d9f654422f586137c4dd726638144e2a09
Dhayana N. Silva <dhayana@on2.dev>
2023-12-04 23:30:34 -0300

    Altera nome do pacote

--- src/main/java/view/Cadastro.java
+++ src/main/java/view/Cadastro.java
@@ -1,4 +1,4 @@
-package views;
+package view;
 
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
--- src/main/java/view/Login.java
+++ src/main/java/view/Login.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package views;
+package view;
 
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
@@ -55,7 +55,8 @@ public class Login extends Janela {
         gbc.gridwidth = 2;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         painel.add(botao_entrar, gbc);
-
+        
         add(painel);
     }
+    
 }
