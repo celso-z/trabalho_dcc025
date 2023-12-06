06a119ca7207a6a9073215aa8e167abbb8cffeb8
Dhayana N. Silva <dhayana@on2.dev>
2023-12-06 15:57:06 -0300

    Atualiza usuarios e filhos

--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -12,16 +12,18 @@ public abstract class Usuario {
 
     private String nome;
     private Integer matricula;
+    private String username;
+    private String senha;
     private static Integer numUsuarios = 0;
-    
-    public Usuario(String nome) {
+
+    public Usuario(String nome, String username, String senha) {
         this.nome = nome;
+        this.username = username;
+        this.senha = senha;
         this.matricula = numUsuarios;
         numUsuarios++;
     }
-    
-    
-    
+
     public String getNome() {
         return nome;
     }
@@ -33,7 +35,7 @@ public abstract class Usuario {
     public static Integer getNumUsuarios() {
         return numUsuarios;
     }
-    
+
     public void setNome(String nome) {
         this.nome = nome;
     }
@@ -45,4 +47,22 @@ public abstract class Usuario {
     public static void setNumUsuarios(Integer numUsuarios) {
         Usuario.numUsuarios = numUsuarios;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
 }
