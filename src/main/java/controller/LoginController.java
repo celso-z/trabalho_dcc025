07f628d74bdeb642a0413fbe5164edf9ed54f139
Dhayana N. Silva <dhayana@on2.dev>
2023-12-08 02:23:05 -0300

    Adiciona verificação de login para funcionario e administrador

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -7,12 +7,16 @@ package controller;
 import model.Cliente;
 import exceptions.DataException;
 import storage.DataManager;
-import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import model.Administrador;
+import view.Adm;
+
+import model.Funcionario;
 import view.Cadastro;
+import view.Func;
 import view.HomeUsuario;
 
 /**
@@ -21,9 +25,40 @@ import view.HomeUsuario;
  */
 public class LoginController {
 
-    public static void entrar(JFrame frame, ActionEvent event, String usuario, String senha) {
+    public static void entrar(JFrame frame, String usuario, String senha) throws DataException {
         List<Cliente> clientes = new ArrayList<>();
+        List<Administrador> administradores = new ArrayList<>();
+        List<Funcionario> funcionarios = new ArrayList<>();
 
+        //Verificação de administrador
+        administradores = DataManager.getFromDisk("Administrador");
+
+        for (Administrador administrador : administradores) {
+            if (administrador != null && administrador.getUsername().equals(usuario) && administrador.getSenha().equals(senha)) {
+                frame.dispose(); //fecha a tela atual
+                new Adm();
+                return;
+            } else if (administrador != null && administrador.getUsername().equals(usuario)) {
+                senhaIncorreta();
+                return;
+            }
+        }
+
+        //Verificação de funcionario
+        funcionarios = DataManager.getFromDisk("Funcionario");
+
+        for (Funcionario funcionario : funcionarios) {
+            if (funcionario != null && funcionario.getUsername().equals(usuario) && funcionario.getSenha().equals(senha)) {
+                frame.dispose(); //fecha a tela atual
+                new Func();
+                return;
+            } else if (funcionario != null && funcionario.getUsername().equals(usuario)) {
+                senhaIncorreta();
+                return;
+            }
+        }
+
+        //Verificação de cliente
         try {
             clientes = DataManager.getFromDisk("Cliente");
         } catch (DataException e) {
@@ -43,9 +78,13 @@ public class LoginController {
 
     }
 
-    public static void telaCadastro(JFrame frame) {
+    private static void telaCadastro(JFrame frame) {
         JOptionPane.showMessageDialog(null, "Usuário não encontrado, você será direcionado para a tela de cadastro!");
         frame.dispose(); //fecha a tela atual
         new Cadastro();
+    }
+
+    private static void senhaIncorreta() {
+        JOptionPane.showMessageDialog(null, "Senha incorreta! Verifique e tente novamente.");
     }
 }
