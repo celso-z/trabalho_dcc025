6c211c94f6b355cb8e78ae256fa6e49d525a6ebb
Celso Zacarias <celso.zacarias@estudante.ufjf.br>
2023-12-11 03:06:19 -0300

    Últimos ajustes estéticos

--- src/main/java/view/TelaCarregar.java
+++ src/main/java/view/TelaCarregar.java
@@ -1,6 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * @author Celso Zacarias da Silva Junior 202076003
+ * @author Dhayana Nascimento Silva 201976040
  */
 package view;
 
